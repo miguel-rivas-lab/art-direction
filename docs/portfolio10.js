74facc6f1bb173681d37a557e5cbb2d0b4230a16
Miguel <jemiguelrivas@gmail.com>
2020-09-13 17:19:29 -0400

    initial commit

---
+++ docs/portfolio10.js
@@ -0,0 +1,83 @@
+[
+	{
+		"number": 1,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 2,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 3,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 4,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 5,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 6,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 7,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 8,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 9,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	}
+]
---
+++ docs/portfolio20.js
@@ -0,0 +1,47 @@
+[
+	{
+		"number": 10,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 11,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 12,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 13,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	},
+	{
+		"number": 14,
+		"pages": 14,
+		"width": 1000,
+		"height": 1000,
+		"fonts": [],
+		"text": [],
+		"detailed": true
+	}
+]
